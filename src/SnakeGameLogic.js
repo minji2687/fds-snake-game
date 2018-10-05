5594e41b89a33817238d90cf6be7f42b79f584a0
Jo Minji <mingi2687@gamil.com>
2018-10-05 16:49:03 +0900

    package 파일 수정

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -6,9 +6,10 @@ import {ROWS, COLS} from './config';
 function SnakeGameLogic() {
   // 각 마디의 좌표를 저장하는 배열
   this.joints = [
+    {x: 4, y: 0},
+    {x: 3, y: 0},
     {x: 2, y: 0},
     {x: 1, y: 0},
-    {x: 0, y: 0},
   ];
 
   // 먹이의 좌표
@@ -16,11 +17,24 @@ function SnakeGameLogic() {
 }
 
 SnakeGameLogic.prototype.up = function() {
+
   // 위쪽 화살표 키를 누르면 실행되는 함수
   console.log('up');
 }
 
 SnakeGameLogic.prototype.down = function() {
+  
+const tail = this.joints.pop()
+
+// 위치를 바꿔준 후
+tail.y =+1
+tail.x =+2
+
+// 머리 앞에 갖다 붙인다!
+this.joints.unshift(tail)
+  
+
+
   // 아래쪽 화살표 키를 누르면 실행되는 함수
   console.log('down');
 }
