1fe23f3c25586c4fcf7f7027fa4f566e7a96d4e9
JoMinJi <35992483+minji2687@users.noreply.github.com>
2018-10-09 18:27:05 +0900

    Update SnakeGameLogic.js

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -1,60 +1,128 @@
-import {ROWS, COLS} from './config';
+import { ROWS, COLS } from "./config";
 
 // NOTE: ROWS, COLS에는 행의 개수, 열의 개수가 저장되어 있습니다.
 // 이 변수를 활용해서 코드를 작성하세요!
 
+// - 설정 파일(config.js)를 편집해 게임 난이도를 바꾸어보세요.
+
 function SnakeGameLogic() {
   // 각 마디의 좌표를 저장하는 배열
-  this.joints = [
-    {x: 4, y: 0},
-    {x: 3, y: 0},
-    {x: 2, y: 0},
-    {x: 1, y: 0},
-  ];
+  this.joints = [{ x: 2, y: 0 }, { x: 1, y: 0 }, { x: 0, y: 0 }];
 
+  // 키보드를 누르지 않아도 이동할 수 있도록 방향 상태를 저장
+  this.direction = "right";
+  
   // 먹이의 좌표
-  this.fruit = {x: 3, y: 5};
+  this.fruit = { x: 3, y: 5 };
 }
 
-SnakeGameLogic.prototype.up = function() {
+SnakeGameLogic.prototype.up = function () {
+  // 방향 상태 변경
+  this.direction = "up";
+};
 
-  // 위쪽 화살표 키를 누르면 실행되는 함수
-  console.log('up');
-}
+SnakeGameLogic.prototype.down = function () {
+  // 방향 상태 변경
+  this.direction = "down";
 
-SnakeGameLogic.prototype.down = function() {
-  
-const tail = this.joints.pop()
+};
 
-// 위치를 바꿔준 후
-tail.y =+1
-tail.x =+2
+SnakeGameLogic.prototype.left = function () {
+  // 방향 상태 변경
+  this.direction = "left";
 
-// 머리 앞에 갖다 붙인다!
-this.joints.unshift(tail)
-  
+};
 
+SnakeGameLogic.prototype.right = function () {
+  // 방향 상태 변경
+  this.direction = "right";
+};
 
-  // 아래쪽 화살표 키를 누르면 실행되는 함수
-  console.log('down');
-}
+SnakeGameLogic.prototype.nextState = function () {
 
-SnakeGameLogic.prototype.left = function() {
-  // 왼쪽 화살표 키를 누르면 실행되는 함수
-  console.log('left');
-}
-
-SnakeGameLogic.prototype.right = function() {
-  // 오른쪽 화살표 키를 누르면 실행되는 함수
-  console.log('right');
-}
-
-SnakeGameLogic.prototype.nextState = function() {
-  // 한 번 움직여야 할 타이밍마다 실행되는 함수
-  // 게임이 아직 끝나지 않았으면 `true`를 반환
-  // 게임이 끝났으면 `false`를 반환
   console.log(`nextState`);
+  // 한 번 움직여야 할 타이밍마다 실행되는 함수
+  let newHead;
+  let newFruit = {};
+
+
+
+  if (this.direction === "up") {
+    newHead = {
+      x: this.joints[0].x,
+      y: this.joints[0].y - 1
+    };
+  } else if (this.direction === "down") {
+    newHead = {
+      x: this.joints[0].x,
+      y: this.joints[0].y + 1
+    };
+  } else if (this.direction === "right") {
+    newHead = {
+      x: this.joints[0].x + 1,
+      y: this.joints[0].y
+    };
+  } else if (this.direction === "left") {
+    newHead = {
+      x: this.joints[0].x - 1,
+      y: this.joints[0].y
+    };
+  }
+
+  if (
+     newHead.y >= ROW || newHead.y < 0 || newHead.x >= COLS || newHead.x < 0 || this.joints.some(joint =>joint.x === newHead.x && joint.y === newHead.y)
+     {
+      return false;
+     }
+
+     if (newHead.x === this.fruit.x && newHead.y === this.fruit.y){
+        do {
+          newFruit.x = Math.floor(Math.random() * COLS);
+          newFruit.y = Math.floor(Math.random() * ROWS);
+          this.fruit = newFruit;
+        } while (
+          (newFruit.x === newHead.x && newFruit.y === newHead.y) || this.joints.some
+            (joint => joint.x === newFruit.x && joint.y === newFruit.y)
+        )
+
+
+
+     }else{
+       this.joins.pop();
+     }
+
+     this.joints.unshift(newHead);
+
+
+
+
+
+  if (
+    newHead.y >= ROWS || 
+    newHead.y < 0 ||
+    newHead.x >= COLS ||
+    newHead.x < 0 ||
+    this.joints.some(joint => joint.x === newHead.x && joint.y === newHead.y)
+  ) {//
+    return false;
+  }
+
+  if (newHead.x === this.fruit.x && newHead.y === this.fruit.y) {
+    do {
+      newFruit.x = Math.floor(Math.random() * COLS);
+      newFruit.y = Math.floor(Math.random() * ROWS);
+      this.fruit = newFruit;
+    } while (
+      (newFruit.x === newHead.x && newFruit.y === newHead.y) || this.joints.some(
+        joint => joint.x === newFruit.x && joint.y === newFruit.y
+      )
+    );
+  } else {
+    this.joints.pop();
+  }
+
+  this.joints.unshift(newHead);
   return true;
-}
+};
 
 export default SnakeGameLogic;
